0e0fbca24fda337ca40bc4bb606eef038e57231d
Aksi0322 <126950051+Aksi0322@users.noreply.github.com>
2024-05-03 10:25:59 +0530

    Create and run the server, Create Database and create user model

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+import env from "dotenv";
+
+mongoose.connect(
+  "mongodb+srv://Akshay0322:Akshay@mern-estate.8dl6wxq.mongodb.net/?retryWrites=true&w=majority&appName=mern-estate"
+).then;
+console.log("Connected to MongoDB");
+
+const app = express();
+app.listen(3000, () => {
+  console.log("Server is running on port 3000..");
+});
+app.get("/test", (req, res) => {
+  res.json({
+    message:'Hello World',
+  });
+});
